57d1a61489fa5472fe791034510b6a591ce86264
vitosotdihaet <vitostikitos@gmail.com>
2022-11-19 16:06:51 +0300

    Change main.c

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-using namespace std;
 #include "stack.hpp"
+
+using namespace std;
 using namespace mystack;
 
 int main() {
     Stack<char> s;
+    s.push('a');
+    s.push('b');
 
     for (uint64_t i = 0; i < s.capacity(); ++i) {
         cout << s.pop() << ' ';
     }
+    cout << '\n';
 
     return 0;
 }
